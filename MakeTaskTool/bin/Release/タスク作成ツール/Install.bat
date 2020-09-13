0b1ffe1c3c3f9a3b84dee7c86f510a3cb92bc8e4
foxtail-kei <pollyanna_kei@yahoo.co.jp>
2020-09-13 11:43:45 +0900

    gitignoreの反映

--- MakeTaskTool/bin/Release/タスク作成ツール/Install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-reg add "HKEY_CLASSES_ROOT\DesktopBackground\Shell\MakeTaskTool" /d "タスクを作成する" /f
-reg add "HKEY_CLASSES_ROOT\DesktopBackground\Shell\MakeTaskTool" /v "Icon" /d "%~dp0MakeTaskTool.ico" /f
-reg add "HKEY_CLASSES_ROOT\DesktopBackground\Shell\MakeTaskTool" /v "Position" /d "Middle" /f
-reg add "HKEY_CLASSES_ROOT\DesktopBackground\Shell\MakeTaskTool\Command" /d "%~dp0MakeTaskTool.exe" /f
-
-echo インストールが完了しました。
-pause
